91dcb3ca3670ae034ea235b0d8f602b5a31b459b
ken-cherry <ken@cherrywebdev.com>
2024-01-15 10:49:01 -0600

    Added: images and data

---
+++ src/data.js
@@ -0,0 +1,35 @@
+import benchPressLg from "./images/benchPressLg.jpg";
+import benchPressMd from "./images/benchPressMd.jpg";
+import deadLiftLg from "./images/deadLiftLg.jpg";
+import deadLiftMd from "./images/deadLiftMd.jpg";
+import freeWeightsLg from "./images/freeWeightsLg.jpg";
+import freeWeightsMd from "./images/freeWeightsMd.jpg";
+import pullUpLg from "./images/pullUpLg.jpg";
+import pullUpMd from "./images/pullUpMd.jpg";
+import treadmillLg from "./images/treadmillLg.jpg";
+import treadmillMd from "./images/treadmillMd.jpg";
+
+export const links = [
+  {
+    id: 1,
+    href: "home",
+    text: "home",
+  },
+  {
+    id: 2,
+    href: "programs",
+    text: "programs",
+  },
+  {
+    id: 3,
+    href: "yourGoals",
+    text: "your goals",
+  },
+  {
+    id: 4,
+    href: "pricing",
+    text: "pricing",
+  },
+];
+
+export const images = [{}];
